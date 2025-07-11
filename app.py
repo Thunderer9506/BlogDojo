57aa1b862ad57199cede30ed299d83d951bd8c2e
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-07-11 13:43:26 +0530

    done with creating new post

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request
+from flask_ckeditor import CKEditor, CKEditorField
+import bleach
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
 from db import db
 from models.user import User
 from models.post import Post
@@ -8,26 +12,38 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['CKEDITOR_PKG_TYPE'] = 'full'
+app.secret_key = 'secret'
 db.init_app(app)
+ckeditor = CKEditor(app)
 
+class PostForm(FlaskForm):
+    content = CKEditorField('Content')
+    submit = SubmitField('Post')
 
-@app.route('/')
+@app.route('/1')
 def home():
-    user = User.query.filter_by(name="Aarav Mehta",password="test1234").first()
+    user = User.query.filter_by(userId = 1).first()
     blogs = []
     for i in json.loads(user.blogId):
         temp = Post.query.filter_by(blogId=i).first()
         blogs.append(temp)
     
-    return render_template('index.html',blogs = blogs)
+    return render_template('index.html',blogs = blogs,id=user.userId)
 
 
+@app.route('/<int:userId>/insert',methods=['GET','POST'])
+def insert(userId):
+    form = PostForm()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = bleach.clean(request.form.get('content'))
+        # You can now store this in a database or print
+        print(f"Title: {title}, Content: {content}")
+        
+    
+    return render_template('newPost.html',id=userId,form=form)
 
 # create the table
 with app.app_context():
     db.create_all()
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
