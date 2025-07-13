362f489a342b001c941da5be048af46e9a15e57d
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-07-13 14:15:07 +0530

    made some changes

--- utils/pfp.py
+++ utils/pfp.py
@@ -4,5 +4,3 @@ def generate_random_pfp(username):
     seeds = ["cat", "robot", "wizard", "ninja", "alien"]
     seed = random.choice(seeds) + username
     return f"https://api.dicebear.com/7.x/adventurer/svg?seed={seed}"
-
-print(generate_random_pfp("afa8c6ae-62ec-4577-9c0e-87dceae09a3c"))
