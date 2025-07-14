d3c7ecf10dc407c55d10c7a2e8cc2d3ae98028a6
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-07-14 23:15:54 +0530

    did some changes for deployment

--- app.py
+++ app.py
@@ -161,4 +161,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
