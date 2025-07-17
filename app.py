43b3bf9bfe44afc4b90204de5b2650175cc9a3a0
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-07-17 10:25:52 +0530

    again some changes

--- app.py
+++ app.py
@@ -159,9 +159,6 @@ def slice_date(s):
     return s.strftime("%d-%m-%Y")
 
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # fallback to 5000 if PORT not set
     app.run(host="0.0.0.0", port=port)
