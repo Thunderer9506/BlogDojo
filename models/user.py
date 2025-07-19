9b2a3ce7aee083c91adf15f84d363032533a91ff
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-07-19 08:47:12 +0530

    updated the size of ids and password

--- models/user.py
+++ models/user.py
@@ -2,10 +2,10 @@ from db import db
 from datetime import datetime,timezone
 
 class User(db.Model):
-    userId = db.Column(db.String(50), primary_key=True)
+    userId = db.Column(db.String(100), primary_key=True)
     profile_pic = db.Column(db.String(150),default="",unique=True)
     name = db.Column(db.String(60), nullable=False)
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime,default=datetime.now(timezone.utc))
