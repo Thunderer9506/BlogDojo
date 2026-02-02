9088e403c3ef7b21f445c72a2c420611a4850e97
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2026-02-02 19:45:31 +0530

    Removed Restriction

--- models/post.py
+++ models/post.py
@@ -2,10 +2,10 @@ from db import db
 from datetime import datetime,timezone
 
 class Post(db.Model):
-    blogId = db.Column(db.String(100), primary_key=True)
-    title = db.Column(db.String(300),nullable=False)
+    blogId = db.Column(db.String(), primary_key=True)
+    title = db.Column(db.String(),nullable=False)
     content = db.Column(db.Text, nullable=False,unique=True)
-    userId = db.Column(db.String(100), nullable=False)
+    userId = db.Column(db.String(), nullable=False)
     date = db.Column(db.DateTime,default=datetime.now(timezone.utc))
     image_data = db.Column(db.LargeBinary)  # Store image binary data
     image_mime = db.Column(db.String(50))   # Optional: store content-type like "image/jpeg"
