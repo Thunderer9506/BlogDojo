45a7cced6cb5cc6ee85284f32437b5d34dd9fdb5
Thunderer9506 <srivastavashaurya9506@gmail.com>
2026-04-07 18:08:48 +0530

    Debug off

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ def slice_date(s):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # fallback to 5000 if PORT not set
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
